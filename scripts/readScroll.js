51cc217369b8f7ce077ba8607710ef5be22138a7
grongo59100 <garrett59100@gmail.com>
2022-10-28 12:24:36 -0400

    first commit

---
+++ scripts/readScroll.js
@@ -0,0 +1,10 @@
+scrollWrapper = document.getElementsByClassName('scroll-wrapper');
+
+scrollWrapper.addEventListener('scroll', () => ({
+
+}));
+
+scrollWrapper.addEventListener('scroll', (event) => {
+    const scrollWrapper = document.getElementsByClassName('scroll-wrapper');
+    scrollWrapper.classList.add('fadeOut') = 0;
+});
